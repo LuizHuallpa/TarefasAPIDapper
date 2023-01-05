105121554609a43f003a039423950a91e71a4488
Luiz Felipe Vicente Nina Huallpa <luiz.huallpa@gmail.com>
2023-01-05 09:52:34 -0300

    Add project files.

---
+++ TarefasAPIDapper/EndPoints/TarefasEndpoints.cs
@@ -0,0 +1,66 @@
+﻿using Dapper.Contrib.Extensions;
+using TarefasAPIDapper.Data;
+using static TarefasAPIDapper.Data.TarefaContext;
+
+namespace TarefasAPIDapper.EndPoints
+{
+    public static class TarefasEndpoints
+    {
+        
+        public static void MapTarefasEndpoints(this WebApplication app){
+            app.MapGet("/", () => $"Bem vindo aTarefas API - { DateTime.Now}");
+
+            app.MapGet("/tarefas", async(GetConnection connectionGetter) =>
+            {
+                using var connection = await connectionGetter();
+                var tarefas = connection.GetAll<Tarefa>().ToList();
+                if(tarefas is null)
+                    return Results.NotFound();
+                return Results.Ok(tarefas);
+            });
+
+
+            app.MapGet("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var connection = await connectionGetter();
+                //var tarefas = connection.Get<Tarefa>(id);
+                //if (tarefas is null)
+                //    return Results.NotFound();
+                //return Results.Ok(tarefas);
+
+                return connection.Get<Tarefa>(id) is Tarefa tarefa ? Results.Ok(tarefa)
+                    : Results.NotFound();
+            });
+
+            app.MapPost("/tarefas", async(GetConnection connectionGetter, Tarefa tarefa) =>
+            {
+                using var connection = await connectionGetter();
+                var id = connection.Insert(tarefa);
+                return Results.Created($"/tarefas/{id}", tarefa);
+            }
+            );
+
+            app.MapPut("/tarefas", async (GetConnection connectionGetter, Tarefa tarefa) =>
+            {
+                using var connection = await connectionGetter();
+                var id = connection.Update(tarefa);
+                return Results.Ok();
+            }
+            );
+            app.MapDelete("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var connection = await connectionGetter();
+                var tarefas = connection.Get<Tarefa>(id);
+                if (tarefas is null)
+                    return Results.NotFound();
+                connection.Delete(tarefas);
+                return Results.Ok(tarefas);
+
+
+            });
+        }
+
+
+
+    }
+}
---
+++ TarefasAPIDapper/Program.cs
@@ -0,0 +1,13 @@
+using TarefasAPIDapper.EndPoints;
+using TarefasAPIDapper.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddPersistence();
+
+
+var app = builder.Build();
+
+app.MapTarefasEndpoints();
+
+app.Run();
